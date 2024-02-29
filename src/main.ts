52efcd986a3c9f19a2bf1205d4e3a9b485b45da7
Laura Pečnik Budna <laura.budna@gmail.com>
2024-02-29 10:19:11 +0100

    add connection to database

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  // // setup to display files
+  // app.use('/files', express.static('files')) // v tem diru bojo vsi file-i.. ustvarjen bo avtomatsko
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+
+  console.log(`App is listening on port ${await app.getUrl()}.`);
+
+  // console.log(`Server listening on port ${PORT}.\nNavigate to http://localhost:${PORT}/`)
 }
 bootstrap();
